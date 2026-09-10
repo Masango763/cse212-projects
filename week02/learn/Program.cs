ab29a411afa0c4bddecc6872b596721f6dea075c
Masango763 <dewhemasango@gmail.com>
2026-09-10 03:22:06 +0200

    Fix duplicate SimpleQueue definitions and successfully run W02 learn tests

--- week02/learn/Program.cs
+++ week02/learn/Program.cs
@@ -1,33 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-
-public class SimpleQueue
-{
-    private readonly List<int> _queue = new List<int>();
-
-    public void Enqueue(int value)
-    {
-        // Requirement: Enqueue adds the item to the back of the queue
-        _queue.Add(value);
-    }
-
-    public int Dequeue()
-    {
-        // Requirement: If empty, throw IndexOutOfRangeException
-        if (_queue.Count == 0)
-        {
-            throw new IndexOutOfRangeException("The queue is empty.");
-        }
-
-        // Requirement: Dequeue removes and returns the item from the front of the queue
-        int value = _queue[0];
-        _queue.RemoveAt(0);
-        return value;
-    }
-
-    public int Count => _queue.Count;
-}
 
 public class Program
 {
